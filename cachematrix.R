ecfdae6d0e446da96dfc5cdfce0dc9ae311b9fc2
etacious <etacious@gmail.com>
2014-04-28 13:45:01 +1000

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,44 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Makes a cache copy of matrix as a 
+## list that includes the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
     
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
-        
-        
-    }
     
-    get <- function() x
-    
-    set_cachemat <- function(solve) m <<- inv #<<-mean
-    get_cachemat <- function() m
-    list(set = set, get = get,
-         set_cachemat =  set_cachemat,
-         get_cachemat = get_cachemat)
-    
+  }
+  
+  get <- function() x
+  
+  set_cachemat <- function(inv) m <<- inv 
+  get_cachemat <- function() m
+  list(set = set, get = get,
+       set_cachemat =  set_cachemat,
+       get_cachemat = get_cachemat)
+  
 }
 
 
 
-## Write a short comment describing this function
+## function for creating an instance of a matrix that
+## calculates its inverse only if no instance in cached data already
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    m <- x$get_cachemat()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
-    }
-    data <- x$get()
-    inv <- solve(data, ...)
-    x$set_cachemat(inv)
-    inv
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$get_cachemat()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$set_cachemat(m)
+  m
 }
-
-m <- matrix( c(2, 4, 3, 1, 5, 7,6,6,5), nrow=3,  ncol=3) 
-m_inv <-solve(m)
